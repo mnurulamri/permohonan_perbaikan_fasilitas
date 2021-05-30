6113bd887818c3073be469336c9f2a709d455277
mnurulamri <mnurulamri@gmail.com>
2021-05-30 18:20:48 +0700

    Create side_menu.php

---
+++ views/side_menu.php
@@ -0,0 +1,61 @@
+<!-- Left side column. contains the logo and sidebar -->
+<aside class="main-sidebar">
+
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+
+        <!-- Sidebar Menu -->
+        <ul class="sidebar-menu" data-widget="tree">
+            <li class="header">TABLE OF CONTENTS</li>
+            <li>
+                <a href="#"><i class="fa fa-microchip"></i> <span> Beranda </span></a>
+            </li>
+            <li>
+                <a class="page" rel="<?=base_url()?>perbaikan/permohonan/identitasDiri"><i class="fa fa-handshake-o"></i>
+                    <span>Identitas Diri</span></a></li>
+            <li>
+                <a class="page" rel="<?=base_url()?>perbaikan/permohonan/data"><i class="fa fa-files-o"></i> <span> Data Permohonan </span></a>
+            </li>
+            
+			<li>
+                <a class="page" rel="<?=base_url()?>perbaikan/persetujuan_operator/data"><i class="fa fa-files-o"></i> <span> Data Permohonan (Operator)</span></a>
+            </li>
+            
+			<li>
+                <a class="page" rel="<?=base_url()?>perbaikan/persetujuan_koordinator/data"><i class="fa fa-files-o"></i> <span> Data Permohonan (Koordinator)</span></a>
+            </li>
+            
+			<li>
+                <a class="page" rel="<?=base_url()?>perbaikan/persetujuan_unit/data"><i class="fa fa-files-o"></i> <span> Data Permohonan (Kepala Unit)</span></a>
+            </li>
+            
+			<li>
+                <a class="page" rel="<?=base_url()?>perbaikan/persetujuan_manajer/data"><i class="fa fa-files-o"></i> <span> Data Permohonan (Manajer)</span></a>
+            </li>
+            
+			<li>
+                <a class="page" rel="<?=base_url()?>perbaikan/persetujuan_wadek/data"><i class="fa fa-files-o"></i> <span> Data Permohonan (Wadek II)</span></a>
+            </li>
+            
+			<!--
+            <li class="treeview active">
+                <a href="#"><i class="fa fa-th"></i> <span>Components</span>
+                    <span class="pull-right-container">
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li>
+                        <a href="/docs/2.4/main-header"><i class="fa fa-circle-o"></i> Main Header</a>
+                    </li>
+                    <li class="active">
+                        <a href="/docs/2.4/sidebar"><i class="fa fa-circle-o"></i> Sidebar</a>
+                    </li>
+                </ul>
+            </li>
+            -->
+        </ul>
+        <!-- /.sidebar-menu -->
+    </section>
+    <!-- /.sidebar -->
+</aside>
